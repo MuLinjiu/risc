9e3461e54ec8cf6925ed813ece91942f3b4e2b44
mulinjiu <1838287508@qq.com>
2021-06-28 15:32:06 +0800

    innit

---
+++ Simulator.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Xialin He on 2021/6/28.
+//
+
+#ifndef RISC_V_SIMULATOR_HPP
+#define RISC_V_SIMULATOR_HPP
+
+#endif //RISC_V_SIMULATOR_HPP
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Xialin He on 2021/6/28.
+//
+#include <iostream>
+#include "Types.hpp"
+using namespace std;
+int main(){
+    cout << "dsg" << endl;
+    return 0;
+}
+
